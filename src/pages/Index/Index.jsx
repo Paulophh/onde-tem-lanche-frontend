ea1aef00777d7dfb0d5d44bff6558619c223d8c3
Mateus Cardoso Passos <mateuscardoso@hotmail.com>
2023-09-05 14:39:11 -0300

    commit

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -44,6 +44,14 @@ const Index = () => {
     restaurants.push(newRestaurant);
   }
 
+  for (let i = 0; i <= 9; i++) {
+    const newDish = {
+      ...mockerDish,
+      id: `dish-${i}`
+    }
+    dishes.push(newDish);
+  }
+
   return (
     <>
       <Header />
